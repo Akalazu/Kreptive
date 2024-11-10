266a5b6361ea7ef89a58c74ca1da0117246fa868
Akalazu <akalazuudochukwu@gmail.com>
2024-11-10 20:15:43 +0100

    dynamic database

--- includes/db.php
+++ includes/db.php
@@ -1,11 +1,11 @@
 <?php
 
-
-try {
-    // $pdo = new PDO('mysql:host=localhost;dbname=u254739106_kreptive_db', 'u254739106_kreptive', '6F/j#+y3WqIf');
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
     $pdo = new PDO('mysql:host=localhost;dbname=mintartes_db', 'root', '');
-
-
+} else {
+    $pdo = new PDO('mysql:host=localhost;dbname=u254739106_kreptive_db', 'u254739106_kreptive', 'i8&tOZQC');
+}
+try {
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {

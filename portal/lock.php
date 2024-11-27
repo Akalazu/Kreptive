e412510bb246fbe5052d08c77c9261b187c703df
Akalazu <akalazuudochukwu@gmail.com>
2024-11-27 10:10:28 +0100

    removed error reporting

--- portal/lock.php
+++ portal/lock.php
@@ -37,7 +37,7 @@
     <script src="../assets/js/jquery.min.js"></script>
 
     <!-- Smartsupp Live Chat script -->
-    <script type="text/javascript">
+    <!-- <script type="text/javascript">
         var _smartsupp = _smartsupp || {};
         _smartsupp.key = '07e8f4cfbd525e6f4e3c33265f93c811bea2424e';
         window.smartsupp || (function(d) {
@@ -53,7 +53,7 @@
             c.src = 'https://www.smartsuppchat.com/loader.js?';
             s.parentNode.insertBefore(c, s);
         })(document);
-    </script>
+    </script> -->
     <noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
 </head>
 

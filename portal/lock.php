3626e8376bc75608b5efc1d1c63815e6d06c31a0
Akalazu <akalazuudochukwu@gmail.com>
2025-01-25 06:37:15 +0100

    Smartupp update'

--- portal/lock.php
+++ portal/lock.php
@@ -38,7 +38,7 @@
 
     <!-- Smartsupp Live Chat script -->
     <!-- Smartsupp Live Chat script -->
-    <script type="text/javascript">
+    <!-- <script type="text/javascript">
         var _smartsupp = _smartsupp || {};
         _smartsupp.key = '5cb72bfceac0248ce873cd52fd5f375ac09b1cdc';
         window.smartsupp || (function(d) {
@@ -54,7 +54,7 @@
             c.src = 'https://www.smartsuppchat.com/loader.js?';
             s.parentNode.insertBefore(c, s);
         })(document);
-    </script>
+    </script> -->
 </head>
 
 
